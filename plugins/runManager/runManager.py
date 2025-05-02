54fdde505916831eb393b3f77d432863c9f28995
Mr.Lee <chzxxuanzheng@gmail.com>
2025-05-02 11:57:07 +0800

    fix: 移除关机指令中的延迟和失败处理逻辑，该逻辑卡死了关机流程

--- plugins/runManager/runManager.py
+++ plugins/runManager/runManager.py
@@ -1,12 +1,8 @@
 from core.matcher import commandMatcher
 from os import kill, getpid
 from signal import SIGINT
-from asyncio import sleep
 
 @commandMatcher('关机', 'poweroff', 'shutdown', desc='关机指令')
 async def poweroff():
 	yield '执行关机...'
 	kill(getpid(), SIGINT)
-	await sleep(10)
-	yield '关机失败，强制关机'
-	exit(1)

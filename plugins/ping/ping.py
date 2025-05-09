fa712ec7c9b0232f698b93fe4a6bfd14f80800f3
Mr.Lee <chzxxuanzheng@gmail.com>
2025-05-09 22:38:07 +0800

    fix: 修复 ping 命令乱匹配规则

--- plugins/ping/ping.py
+++ plugins/ping/ping.py
@@ -1,5 +1,14 @@
 from core.matcher import commandMatcher
+from util.rule import norRule
 
-@commandMatcher('ping', 'p', desc='ping')
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+
+async def noArgs(msg: Message = CommandArg())-> bool:
+	if len(msg) == 0:
+		return True
+	return False
+
+@commandMatcher('ping', 'p', desc='ping', rule=norRule & noArgs)
 async def ping():
 	yield 'pong'

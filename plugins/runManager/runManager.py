87a58e2b9a804208c01e107282de3c53ff74fc5f
Mr.Lee <chzxxuanzheng@gmail.com>
2025-05-10 10:17:15 +0800

    fix: 修复运行状况管理，无权限人人可以运行的bug

--- plugins/runManager/runManager.py
+++ plugins/runManager/runManager.py
@@ -2,7 +2,9 @@ from core.matcher import commandMatcher
 from os import kill, getpid
 from signal import SIGINT
 
-@commandMatcher('关机', 'poweroff', 'shutdown', desc='关机指令')
+from util.rule import fromSu, norRule
+
+@commandMatcher('关机', 'poweroff', 'shutdown', rule=norRule & fromSu, desc='关机指令')
 async def poweroff():
 	yield '执行关机...'
 	kill(getpid(), SIGINT)
